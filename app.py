659a949ec84eaac5d1ebd431a61b0e18701e8fdc
csm <csm009177@gmail.com>
2024-03-13 19:39:51 +0900

    app.py - link with Fetches.tsx

---
+++ app.py
@@ -0,0 +1,32 @@
+# app.py
+
+# FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# FastAPI 애플리케이션 생성
+app = FastAPI()
+
+# CORS 설정 허용할 주소
+origins = [
+    "http://localhost:3000",  # Next.js 애플리케이션의 주소
+    "http://127.0.0.1:3000",  # Next.js 애플리케이션의 주소
+]
+
+# CORS 미들웨어 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Greet": "Hello, FastAPI!" }
+
+# FastAPI 서버 실행
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,35 +1,11 @@
 'use client'
-import { useEffect, useState } from "react";
+import Fetches from './Fetches';
 
 export default function Home() {
     // API 서버로부터 데이터를 가져오는 함수
-    const [APIData, setAPIData] = useState('')
-    const fetchAPIData = () => {
-      fetch('http://localhost:8000/') // endpoint와 포트 번호를 지정
-      .then((response) => {
-          // 응답을 JSON 형태로 변환
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        }).
-        then((data)=>{
-          // 변환된 데이터를 처리 (콘솔에 출력)
-          setAPIData(data);
-          console.log(data);
-        })
-        .catch((error)=> {
-          // 오류 발생 시 처리 ()
-          console.error('There was a problem with your fetch operation:', error);
-        })
-    }
-useEffect(()=>{
-  fetchAPIData()
-}, [APIData])
-
   return (
     <main>
-      <button onClick={fetchAPIData}>fetchAPIData</button>  
+      <Fetches/>
     </main>
   );
 }
